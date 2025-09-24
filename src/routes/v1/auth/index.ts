36e542ab13e726424ac856b99881b9737ce0ca0c
Zanzibar <zanzisworld42@gmail.com>
2025-09-23 20:55:06 -0700

    Set up database, migrations

--- src/routes/v1/auth/index.ts
+++ src/routes/v1/auth/index.ts
@@ -1,3 +1,6 @@
+import { FastifyPluginAsync } from 'fastify'
+
+
 interface IQuerystring {
   username: string;
   password: string;
@@ -12,3 +15,20 @@ interface IReply {
   302: { url: string };
   '4xx': { error: string };
 }
+
+const authRoutes: FastifyPluginAsync = async (fastify, opts) => {
+  fastify.get<{
+    Querystring: IQuerystring;
+    Headers: IHeaders;
+    Reply: IReply;
+  }>('/', async (request, reply) => {
+    const { username, password } = request.query
+    const hCustom = request.headers['h-Custom']
+
+    fastify.log.info(`Username: ${username}, Password: ${password}, Custom header: ${hCustom}`)
+
+    reply.code(200).send({ success: true })
+  })
+}
+
+export default authRoutes
