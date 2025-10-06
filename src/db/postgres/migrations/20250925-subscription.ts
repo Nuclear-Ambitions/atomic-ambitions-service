8f1e3c970dc384050d5c31426036e65e35ff5bba
Zanzibar <zanzisworld42@gmail.com>
2025-10-06 08:29:11 -0700

    Revise data model for stripe subscriptions

--- src/db/postgres/migrations/20250925-subscription.ts
+++ src/db/postgres/migrations/20250925-subscription.ts
@@ -5,21 +5,16 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn("stripe_customer_id", "text")
     .execute()
 
-  await db.schema.createTable('products')
-    .addColumn("id", "uuid", col => col.notNull().unique())
-    .addColumn("name", "text")
-    .addColumn("stripe_product_id", "text")
-    .addColumn("stripe_price_id", "text")
-    .addColumn("interval", "text")
-    .addColumn("price", "float4")
-    .addColumn("currency", "text")
-    .execute()
-
-  await db.schema.createTable('subscriptions')
-    .addColumn("id", "uuid", col => col.notNull().unique())
+  await db.schema.createTable('stripe_subscriptions')
+    .addColumn("id", "text", col => col.notNull().unique())
+    .addColumn("stripe_customer_id", "text")
     .addColumn("user_id", "uuid", col => col.references('users.id').onDelete('cascade'))
-    .addColumn("product_id", "uuid", col => col.references('products.id').onDelete('cascade'))
-    .addColumn("stripe_subscription_id", "text")
+    .addColumn("customer_email", "text")
+    .addColumn("plan_price_id", "int4")
+    .addColumn("plan_product_id", "text")
+    .addColumn("plan_amount", "int4")
+    .addColumn("plan_interval", "text")
+    .addColumn("payment_status", "text")
     .addColumn("status", "text")
     .addColumn('current_period_start', 'timestamptz')
     .addColumn('current_period_end', 'timestamptz')
@@ -28,24 +23,17 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn('updated_at', 'timestamptz', (col) => col.defaultTo(sql`now()`).notNull())
     .execute()
 
-  await db.schema.createTable('payments')
-    .addColumn("id", "uuid", col => col.notNull().unique())
-    .addColumn("subscription_id", "uuid", col => col.references('subscriptions.id').onDelete('cascade'))
-    .addColumn("stripe_payment_intent_id", "text")
-    .addColumn("amount", "float4")
-    .addColumn("currency", "text")
-    .addColumn('created_at', 'timestamptz', (col) => col.defaultTo(sql`now()`).notNull())
+  await db.schema.createTable('stripe_sessions')
+    .addColumn("id", "text", col => col.notNull().unique())
+    .addColumn("payload", "json")
     .execute()
 
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema.dropTable('payments').ifExists().execute()
-  await db.schema.dropTable('subscriptions').ifExists().execute()
-  await db.schema.dropTable('products').ifExists().execute()
+  await db.schema.dropTable('stripe_subscriptions').ifExists().execute()
+  await db.schema.dropTable('stripe_sessions').ifExists().execute()
   await db.schema.alterTable('users')
     .dropColumn('stripe_customer_id')
     .execute()
 }
-
-// TODO: list use cases; sus out data model
